f831bba9b05db0a3ff3354a02b7686ce1768046f
mukesh4nitrate <143835380+mukesh4nitrate@users.noreply.github.com>
2023-10-05 23:06:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-# Leap year
-"""
-year % 4 == 0 &
-year % 100 != 0 /
-year % 400 == 0
+class BankAccount:
+  def __init__(self, account_number, account_holder_name,initial_balance=0.0):
+    self.__account_number =    account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance =            initial_balance
+  def deposit(self, amount):
+    if amount>0:
+      self.__account_balance +=    amount
+      print("Deposited ₹{}. New balance:₹{}".format(amount, 
+                                                           self.__account_balance))
+    else:
+      print("Invalid deposit      amount.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance-=amount
+      print("withdrew ₹{}. New balance: ₹{}". format(amount, 
+                                                           self.__account_balance))
+    else:
+      print("invalid withdrawal    amount or insufficient balance.")
+  def display_balance(self):
+    print("Account balance for        {} (Account #{}): ₹{}".format( 
+               self.__account_holder_name, self.__account_number, 
+        self.__account_balance))
+account= BankAccount(account_number="123456789",
+                                              account_holder_name="Mukesh",        
+                                              initial_balance=5000.0)  
 
-"""
+#Test deposit and withdrawal functionality
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
+      
 
 
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
 
-
-year = 2012
-
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
